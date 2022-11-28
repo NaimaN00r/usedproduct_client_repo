14c9923ad533a7f7a5a552155da2d1d614a15899
NaimaN00r <anaima407@gmail.com>
2022-11-28 21:55:52 +0600

    19th commit

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,41 +1,41 @@
-// import { Elements } from '@stripe/react-stripe-js';
-// import { loadStripe } from '@stripe/stripe-js';
-// import React, { useContext } from 'react';
-// import { useLoaderData } from 'react-router';
-// import { AuthContext } from '../../../contexts/AuthProvider';
-// // import { useNavigation } from 'react-day-picker';
-// import Loading from '../../../Pages/Shared/Loading/Loading';
-// import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import React, { useContext } from 'react';
+import { useLoaderData } from 'react-router';
+import { AuthContext } from '../../../contexts/AuthProvider';
+// import { useNavigation } from 'react-day-picker';
+import Loading from '../../../Pages/Shared/Loading/Loading';
+import CheckoutForm from './CheckoutForm';
 
 
-// const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PK}`);
-// console.log(stripePromise);
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PK}`);
+console.log(stripePromise);
 
-// const Payment = () => {
-//     const {isLoading}=useContext(AuthContext);
-//     const booking = useLoaderData();
-//     console.log(booking);
-//     // const navigation = useNavigation();
-//     const { treatment, price, appointmentDate, slot } = booking;
-//     // if(navigation.state === "loading"){
-//     //     return <Loading></Loading>
-//     // }
-//     if(isLoading){
-//         <Loading></Loading>
-//     }
-//     return (
-//         <div>
-//             <h3 className="text-3xl">Payment for {treatment}</h3>
-//             <p className="text-xl">Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}</p>
-//             <div className='w-96 my-12'>
-//                 <Elements stripe={stripePromise}>
-//                     <CheckoutForm
-//                         booking={booking}
-//                     />
-//                 </Elements>
-//             </div>  
-//         </div>
-//     );
-// };
+const Payment = () => {
+    const {isLoading}=useContext(AuthContext);
+    const booking = useLoaderData();
+    console.log(booking);
+    // const navigation = useNavigation();
+    const { treatment, price, appointmentDate, slot } = booking;
+    // if(navigation.state === "loading"){
+    //     return <Loading></Loading>
+    // }
+    if(isLoading){
+        <Loading></Loading>
+    }
+    return (
+        <div>
+            <h3 className="text-3xl">Payment for {treatment}</h3>
+            <p className="text-xl">Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}</p>
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                        booking={booking}
+                    />
+                </Elements>
+            </div>  
+        </div>
+    );
+};
 
-// export default Payment;
+export default Payment;
