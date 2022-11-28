c1defb3baac5f4609d4032b47866555edb49dde7
NaimaN00r <anaima407@gmail.com>
2022-11-28 21:58:43 +0600

    24th commit

--- src/Pages/Home/Categorie/Categories/SubCategories.js
+++ src/Pages/Home/Categorie/Categories/SubCategories.js
@@ -1,50 +1,50 @@
-// import React, { useContext, useState } from 'react';
-// import { useLoaderData } from 'react-router';
-// import { AuthContext } from '../../../../contexts/AuthProvider';
-// import BookingModal from '../../../Booking/BookingModal/BookingModal';
-// import Loading from '../../../Shared/Loading/Loading';
+import React, { useContext, useState } from 'react';
+import { useLoaderData } from 'react-router';
+import { AuthContext } from '../../../../contexts/AuthProvider';
+import BookingModal from '../../../Booking/BookingModal/BookingModal';
+import Loading from '../../../Shared/Loading/Loading';
 
-// import CategoryCard from './CategoryCard';
+import CategoryCard from './CategoryCard';
 
-// const SubCategories = () => {
-//     const [products, setproducts] = useState(useLoaderData());
-//     const [Product, setProduct] = useState(null);
-//     const {isLoading}=useContext(AuthContext);
-//     console.log(Product);
-//     if(isLoading){
-//         <Loading></Loading>
-//     }
-//     return (
-//         <div>
-//             <div className='text-center mb-4'>
+const SubCategories = () => {
+    const [products, setproducts] = useState(useLoaderData());
+    const [Product, setProduct] = useState(null);
+    const {isLoading}=useContext(AuthContext);
+    console.log(Product);
+    if(isLoading){
+        <Loading></Loading>
+    }
+    return (
+        <div>
+            <div className='text-center mb-4'>
                
-//                 <h2 className="text-5xl font-semibold pt-12">Resell Products</h2>
+                <h2 className="text-5xl font-semibold pt-12">Resell Products</h2>
                 
-//             </div>
-//             <div className='pl-5 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-//                 {
-//                     products.map(product => !product.paid &&
+            </div>
+            <div className='pl-5 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    products.map(product => !product.paid &&
                         
-//                         <CategoryCard
-//                         key={product._id}
-//                         product={product}
-//                         setProduct={setProduct}
-//                     ></CategoryCard>
+                        <CategoryCard
+                        key={product._id}
+                        product={product}
+                        setProduct={setProduct}
+                    ></CategoryCard>
                     
-//                     )
-//                 }
-//             </div>
-//            {
-//                 Product &&
-//                 <BookingModal
-//                 Product={Product}
-//                 setProduct={setProduct}
+                    )
+                }
+            </div>
+           {
+                Product &&
+                <BookingModal
+                Product={Product}
+                setProduct={setProduct}
 
-//             ></BookingModal>
-//             }
+            ></BookingModal>
+            }
            
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default SubCategories;
+export default SubCategories;
